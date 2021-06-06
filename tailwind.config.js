3fd3bb528b35c09315c7ad5b1c9e1d9457784c05
Michael Wagner <mdwranger@gmail.com>
2021-06-06 00:05:32 -0400

    chore: tailwind breakpoints match ionic

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   purge: ["src/**/*.{js,jsx,ts,tsx}", "public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      xs: "0",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
   },
   variants: {
     extend: {},
