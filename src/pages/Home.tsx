622dd0566507c8aa23fee3a05a716e87b558a5e2
Michael Wagner <mdwranger@gmail.com>
2021-06-05 23:28:25 -0400

    chore: tailwindcss

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,6 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
-import "./Home.css";
 
 const Home: React.FC = () => {
   return (
@@ -22,7 +20,7 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <div className="p-4 text-xl">Hello World!</div>
       </IonContent>
     </IonPage>
   );
